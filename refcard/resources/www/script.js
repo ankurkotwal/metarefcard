9a19d1d91f827a8ba2ea0cd162bb03dcec6510b1
Ankur Kotwal <ankur.kotwal@gmail.com>
2020-09-26 17:40:56 +1000

    Copy FS2020 path to clipboard

--- refcard/resources/www/script.js
+++ refcard/resources/www/script.js
@@ -1,3 +1,34 @@
+// A minimal polyfill for `navigator.clipboard.writeText()` that works most of the time in most modern browsers.
+// Note that on Edge this may call `resolve()` even if copying failed.
+// See https://github.com/lgarron/clipboard-polyfill for a more robust solution.
+// License: public domain
+function copyTextFromElement(element) {
+  return new Promise(function (resolve, reject) {
+
+    /********************************/
+    var range = document.createRange();
+    range.selectNodeContents(document.body);
+    document.getSelection().addRange(range);
+    /********************************/
+
+    var success = false;
+    function listener(e) {
+      e.clipboardData.setData("text/plain", element.innerHTML);
+      e.preventDefault();
+      success = true;
+    }
+    document.addEventListener("copy", listener);
+    document.execCommand("copy");
+    document.removeEventListener("copy", listener);
+
+    /********************************/
+    document.getSelection().removeAllRanges();
+    /********************************/
+
+    success ? resolve() : reject();
+  });
+};
+
 $(function () {
   let inputFile = $('#filesInput');
   let addButton = $('#addButton');
