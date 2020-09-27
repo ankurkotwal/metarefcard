03a742cff6293ec656b322ac115418e413c5bf05
Ankur Kotwal <ankur.kotwal@gmail.com>
2020-09-27 17:03:05 +1000

    Initial web page work for more games. Class form-group causing issues.

--- refcard/resources/www/script.js
+++ refcard/resources/www/script.js
@@ -30,73 +30,77 @@ function copyTextFromElement(element) {
 };
 
 $(function () {
-  let inputFile = $('#filesInput');
-  let addButton = $('#addButton');
-  let generateButton = $('#generateButton');
-  let filesContainer = $('#files');
   let files = [];
 
-  inputFile.change(function () {
-    generateButton.attr("disabled", true);
-    let newFiles = [];
-    for (let index = 0; index < inputFile[0].files.length; index++) {
-      generateButton.attr("disabled", false);
-      let file = inputFile[0].files[index];
-      newFiles.push(file);
-      files.push(file);
-    }
-
-    newFiles.forEach(file => {
-      let fileElement = $(`<option>${file.name}</option>`);
-      fileElement.data('fileData', file);
-      filesContainer.append(fileElement);
-
-      fileElement.click(function (event) {
-        let fileElement = $(event.target);
-        let indexToRemove = files.indexOf(fileElement.data('fileData'));
-        fileElement.remove();
-        files.splice(indexToRemove, 1);
-
-        if (filesContainer.children().length == 0) {
-          generateButton.attr("disabled", true);
-        } else {
-          generateButton.attr("disabled", false);
-        }
-      });
-    });
+  // ##### Flight Simulator 2020 #####
+  let fs2020InputFile = $('#fs2020FilesInput');
+  let fs2020AddButton = $('#fs2020AddButton');
+  let fs2020GenerateButton = $('#fs2020GenerateButton');
+  let fs2020FilesContainer = $('#fs2020Files');
+  fs2020InputFile.change(function () {
+    inputFileChange(fs2020GenerateButton, fs2020InputFile, files, fs2020FilesContainer);
     $(this).val('') // Makes it possible to add, remove, add same file
   });
-
-  addButton.click(function () {
-    inputFile.click();
+  fs2020AddButton.click(function () {
+    fs2020InputFile.click();
   });
-
-  generateButton.click(function () {
-    $('#images').empty();
-    $('#progressbar').show();
-    let formData = new FormData();
-
-    files.forEach(file => {
-      formData.append('file', file);
-    });
-
-    $.ajax({
-      url: '/fs2020',
-      data: formData,
-      type: 'POST',
-      success: function (data) {
-        $('#progressbar').hide();
-        imageContainer = $('#images');
-        imageContainer.empty();
-        imageContainer.html(data);
-      },
-      error: function (data) {
-        $('#progressbar').hide();
-        console.log('ERROR !!!');
-      },
-      cache: false,
-      processData: false,
-      contentType: false
-    });
+  fs2020GenerateButton.click(function () {
+    callBackend('/fs2020', files, $('#fs2020Progressbar'), $('#fs2020Images'))
   });
 });
+
+function inputFileChange(generateButton, inputFile, files, filesContainer) {
+  generateButton.attr("disabled", true);
+  let newFiles = [];
+  for (let index = 0; index < inputFile[0].files.length; index++) {
+    generateButton.attr("disabled", false);
+    let file = inputFile[0].files[index];
+    newFiles.push(file);
+    files.push(file);
+  }
+
+  newFiles.forEach(file => {
+    let fileElement = $(`<option>${file.name}</option>`);
+    fileElement.data('fileData', file);
+    filesContainer.append(fileElement);
+
+    fileElement.click(function (event) {
+      let fileElement = $(event.target);
+      let indexToRemove = files.indexOf(fileElement.data('fileData'));
+      fileElement.remove();
+      files.splice(indexToRemove, 1);
+
+      if (filesContainer.children().length == 0) {
+        generateButton.attr("disabled", true);
+      } else {
+        generateButton.attr("disabled", false);
+      }
+    });
+  });
+}
+
+function callBackend(url, files, progressbar, imageContainer) {
+  let formData = new FormData();
+  files.forEach(file => {
+    formData.append('file', file);
+  });
+
+  imageContainer.empty();
+  progressbar.show();
+  $.ajax({
+    url: url,
+    data: formData,
+    type: 'POST',
+    success: function (data) {
+      progressbar.hide();
+      imageContainer.html(data);
+    },
+    error: function (data) {
+      progressbar.hide();
+      console.log('ERROR !!!');
+    },
+    cache: false,
+    processData: false,
+    contentType: false
+  });
+}
