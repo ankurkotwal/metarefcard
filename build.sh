ae8370f52d67c8b53d7a22375ef4ce4d39e7c23d
ankur.kotwal <ankur.kotwal@gmail.com>
2024-12-14 12:30:04 +0000

    Improve build script syntax

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ `uname` == "Darwin" ] && [ -n "$HOMEBREW_PREFIX" ]; then
+if [[ `uname` == "Darwin" ]] && [[ -n "$HOMEBREW_PREFIX" ]]; then
     export CGO_CFLAGS="-I ${HOMEBREW_PREFIX}/include"
     export CGO_LDFLAGS="-L ${HOMEBREW_PREFIX}/lib"
 fi
